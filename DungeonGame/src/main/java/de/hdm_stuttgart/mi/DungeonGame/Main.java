9a346b31744cb6eeb07f21a44e5f950924306a75
dingi24 <moritz.wahlenmeier@yahoo.de>
2019-11-26 19:16:39 +0100

    Initial Commit
    Created and Added Intellij Maven Project and gitignore files

---
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Main.java
@@ -0,0 +1,17 @@
+package de.hdm_stuttgart.mi.DungeonGame;
+
+/**
+ * A simple HelloWorld style application.
+ */
+
+public class Main {
+
+    /**
+     * Your application's main entry point.
+     *
+     * @param args Yet unused
+     */
+    public static void main( String[] args ) {
+
+    }
+}
