4cb41f2f77724b468208050339e3657c3afd47df
Sascha Wirtz <58302057+TrickyTreat@users.noreply.github.com>
2019-12-13 09:56:18 +0100

    Create Field.java
    Created Filed.java basis.

---
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Stages/Field.java
@@ -0,0 +1,18 @@
+/*
+ * Field.java
+ *
+ * Main class for field creation and managing.
+ *
+ * author: Sascha W.
+ * last edit / by: 2019-12-13 / Sascha W.
+ */
+package de.hdm_stuttgart.mi.DungeonGame.Logics.Stages;
+
+//Import statements
+
+/**
+ * Junction of all Field related classes.
+ */
+public class Field {
+
+}
