c4144571b478cb980277bc67416071106138a36c
Sascha Wirtz <58302057+TrickyTreat@users.noreply.github.com>
2019-12-14 14:34:13 +0100

    Revert "adding too FieldTypes.java and Field.java"
    This reverts commit 3ae599871dbb609c5946b5ca24a4b5d0fbe5e452.

--- DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Stages/Enum/FieldTypes.java
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Stages/Enum/FieldTypes.java
@@ -13,7 +13,7 @@ package de.hdm_stuttgart.mi.DungeonGame.Logics.Stages.Enum;
 /**
  * List of usable FieldTypes (Wall, Water, ...).
  */
-public enum FieldType {
+public enum FieldTypes {
     /**
      * Wall is a FiledType which shapes the basic shape of the Field and creates simple walls to interfere with the players path.
      */
@@ -28,17 +28,7 @@ public enum FieldType {
      * Water is a FieldType which creates another interference for the player.
      * Only used in prebuild Fields.
      */
-    Water(false),
-
-    /**
-     * Door is a FieldType which is accessible and leads to a Field change.
-     */
-    Door(true),
-
-    /**
-     * Stairs are a FieldType which are accessible and are leading to a Field change.
-     */
-    Stairs(true);
+    Water(false);
 
     /**
      * Adding a boolean to check the FieldTypes accessibility.
@@ -50,7 +40,7 @@ public enum FieldType {
      *
      * @param walkable setting if its possible to walk on this particular FieldType.
      */
-    private FieldType(boolean walkable){
+    private FieldTypes(boolean walkable){
         this.walkable = walkable;
     }
 }
--- DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Stages/Field.java
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Stages/Field.java
@@ -10,81 +10,9 @@ package de.hdm_stuttgart.mi.DungeonGame.Logics.Stages;
 
 //Import statements
 
-import de.hdm_stuttgart.mi.DungeonGame.Logics.Stages.Enum.FieldType;
-
 /**
  * Junction of all Field related classes.
  */
 public class Field {
 
-    //Attributes
-
-    //random FieldType hight
-    int hight = (int) Math.random() * 45 + 5;
-    //random FieldType width
-    int width = (int) Math.random() * 45 + 5;
-    //new door x-coordinate
-    int doorXCoordinate;
-    //new door y-coordinate
-    int doorYCoordinate;
-
-    /**
-     * Constructor to feed new door coordinates.
-     * @param doorXCoordinate is the new x-coordinate for door position.
-     * @param doorYCoordinate is the new y-coordinate for door position.
-     */
-    public Field(int doorXCoordinate, int doorYCoordinate){
-        this.doorXCoordinate = doorXCoordinate;
-        this.doorYCoordinate = doorYCoordinate;
-    }
-
-    /**
-     * FieldType is the main array in which the end Field will be saved.
-     */
-    FieldType[][] room = new FieldType[hight][width];
-
-    /**
-     * Passes on the x-coordinate for the new door in next field.
-     *
-     * @return x-coordinate of the new door.
-     */
-    public int getDoorXCoordinate() {
-        return doorXCoordinate;
-    }
-
-    /**
-     * Reciving new x-coordinte for door / spawn.
-     *
-     * @param doorXCoordinate is setting the new door x-coordinate.
-     */
-    public void setDoorXCoordinate(int doorXCoordinate) {
-        this.doorXCoordinate = doorXCoordinate;
-    }
-
-    /**
-     * Passes on the y-coordinate for the new door in next field.
-     *
-     * @return y-coordinate of the new door.
-     */
-    public int getDoorYCoordinate() {
-        return doorYCoordinate;
-    }
-
-    /**
-     * Reciving new x-coordinate for new door / spawn.
-     *
-     * @param doorYCoordinate is setting the new door y-coordinte.
-     */
-    public void setDoorYCoordinate(int doorYCoordinate) {
-        this.doorYCoordinate = doorYCoordinate;
-    }
-
-    /**
-     * Returns FieldType for Field-, Player-interactions or rendering.
-     *
-     * @return a specific FieldType.
-     */
-    public FieldType[][] getRoom() {
-        return room;
-    }
 }
