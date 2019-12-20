8b23c4ef43d2f760c9b98176152bc028d67b19fc
Sascha Wirtz <58302057+TrickyTreat@users.noreply.github.com>
2019-12-20 12:50:15 +0100

    Added another Class layer
    renamed Field.java to Room.java
    added Field.java to control Player and rooms in total

---
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Stages/Room.java
@@ -0,0 +1,120 @@
+/*
+ * Room.java
+ *
+ * Class for room creation and managing.
+ *
+ * author: Sascha W.
+ * last edit / by: 2019-12-20 / Sascha W.
+ */
+package de.hdm_stuttgart.mi.DungeonGame.Logics.Stages;
+
+//Import statements
+import de.hdm_stuttgart.mi.DungeonGame.Helper.Stages.CreateWallAndFloor;
+import de.hdm_stuttgart.mi.DungeonGame.Helper.Stages.Entry;
+import de.hdm_stuttgart.mi.DungeonGame.Helper.Stages.PutEntryAndExit;
+import de.hdm_stuttgart.mi.DungeonGame.Logics.Actors.Enemy;
+import de.hdm_stuttgart.mi.DungeonGame.Logics.Enum.Difficulty;
+import de.hdm_stuttgart.mi.DungeonGame.Logics.Stages.Enum.FieldType;
+
+import java.util.ArrayList;
+
+/**
+ * Junction of all Field related classes.
+ */
+public class Room {
+
+    //Attributes
+
+    //random FieldType hight
+    private final int HIGHT = (int) (Math.random() * 25) + 6;
+    //random FieldType width
+    private final int WIDTH = (int) (Math.random() * 45) + 6;
+    //new entry
+    private Entry entry;
+    //List with all entrys
+    private ArrayList<Entry> doorsAndStairs = new ArrayList<Entry>();
+    //random roomType
+    private final int ROOMTYPE = (int) (Math.random() * 100);
+    //Difficulty
+    private final Difficulty DIFFICULTY;
+    //List of all Enemies
+    private ArrayList<Enemy> enemies;
+
+    /**
+     * FieldType is the main array in which the end Field will be saved.
+     */
+    private FieldType[][] room = new FieldType[HIGHT][WIDTH];
+
+    /**
+     * Constructor to Field.
+     *
+     * @param entry is the new direction for entry position.
+     */
+    public Room(Difficulty difficulty, Entry entry) {
+        this.DIFFICULTY = difficulty;
+        this.entry = entry;
+
+        //Fill room-array with simple wall and floor
+        new CreateWallAndFloor(room);
+
+        /**
+         * Selection of roomtypes.
+         */
+        if(ROOMTYPE <= 100 && ROOMTYPE >= 0) {
+            //Adding entry and exit
+            new PutEntryAndExit(entry, room, doorsAndStairs);
+        //ToDo: Different map genertors
+        }
+    }
+
+    /**
+     * Refresh room with new Player and Enemy Coordinates
+     */
+    /*
+    public void refreshRoom() {
+
+    }
+*/
+    public void moveActor() {
+
+    }
+
+    /**
+     * Method to trigger other mechanics
+     */
+    /*
+    public void checkPlayerField() {
+        //ToDo:Item and Exit check
+    }
+     */
+
+    /**
+     * Passes on the EntryDirection for the next field.
+     *
+     * @return dirction of the new entry.
+     */
+    public Entry getEntry() {
+        return entry;
+    }
+
+
+    /**
+     * Returns FieldType for Field-, Player-interactions or rendering.
+     *
+     * @return a specific FieldType.
+     */
+    public FieldType[][] getRoom() {
+        return room;
+    }
+
+    /**
+     * Returns a requested FieldType
+     *
+     * @param xCoordinate of requested tile
+     * @param yCoordinate of requested tile
+     * @return FieldType of requested tile
+     */
+    public FieldType getFieldType(int xCoordinate, int yCoordinate) {
+        return room[yCoordinate][xCoordinate];
+    }
+}
