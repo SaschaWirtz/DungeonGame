e0b4c986ef7ed220072a0252644b0d30b2260c3e
Sascha Wirtz <58302057+TrickyTreat@users.noreply.github.com>
2020-01-15 18:59:32 +0100

    Update Console.java
    fix console clear for Windows
    by Andreas

--- DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Helper/Graphics/Console.java
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Helper/Graphics/Console.java
@@ -4,11 +4,12 @@
  * A static class with helper methods for console interaction
  *
  * author: Andreas G.
- * last edit / by: 2019-12-06 / Andreas G.
+ * last edit / by: 2020-01-15 / Andreas G.
  */
 package de.hdm_stuttgart.mi.DungeonGame.Helper.Graphics;
 
 //Import statements
+import java.io.IOException;
 
 /**
  * A static class with helper methods for console interaction
@@ -18,7 +19,11 @@ public class Console {
      * Clearing the current console screen
      */
     public static void clear() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException ex) {}
     }
 }
