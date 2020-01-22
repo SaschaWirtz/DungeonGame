cb3c8461d75460268290f66dccbc076991c222dd
Sascha Wirtz <sw210@hdm-stuttgart.de>
2020-01-22 19:10:13 +0100

    changes in CreateItems.java

--- DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Helper/Logics/Stages/CreateItems.java
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Helper/Logics/Stages/CreateItems.java
@@ -9,6 +9,7 @@
 package de.hdm_stuttgart.mi.DungeonGame.Helper.Logics.Stages;
 
 //Import statements
+import de.hdm_stuttgart.mi.DungeonGame.Helper.Logics.Actors.Enums.PotionType;
 import de.hdm_stuttgart.mi.DungeonGame.Helper.Logics.Actors.Potion;
 import de.hdm_stuttgart.mi.DungeonGame.Helper.Logics.Coordinate;
 import de.hdm_stuttgart.mi.DungeonGame.Logics.Stages.Enum.FieldType;
@@ -23,12 +24,13 @@ public class CreateItems {
      */
     public static ArrayList fillItemList(FieldType[][] room){
         ArrayList<Potion> items = new ArrayList<>();
-//        ArrayList<Coordinate> availableTiles = FreeTiles.freeTiles(room);
-//        for(int itemcount = 0; itemcount < FreeTiles.freeTiles(room).size() / 15; itemcount++) {
-//            int itemCoordinate = (int) (Math.random() * availableTiles.size());
-//            items.add(new Potion());
-//            availableTiles.remove(itemCoordinate);
-//        }
+        ArrayList<Coordinate> availableTiles = FreeTiles.freeTiles(room);
+        for(int itemcount = 0; itemcount < FreeTiles.freeTiles(room).size() / 15; itemcount++) {
+            int itemCoordinate = (int) (Math.random() * availableTiles.size());
+            PotionType potiont;
+//            items.add(new Potion(PotionType.values()[(int) ((Math.random() * PotionType.values().length)], ));
+            availableTiles.remove(itemCoordinate);
+        }
         return items;
     }
 }
