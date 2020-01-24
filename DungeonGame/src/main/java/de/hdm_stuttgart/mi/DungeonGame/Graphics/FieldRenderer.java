b83515767fa4db404d1417f656738c7fd4a6af8d
Sascha Wirtz <sw210@hdm-stuttgart.de>
2020-01-24 16:37:26 +0100

    created a FieldRenderer class

---
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Graphics/FieldRenderer.java
@@ -0,0 +1,26 @@
+/*
+ * FieldRenderer.java
+ *
+ * Renderer responsible for generating the field graphics
+ *
+ * author: Sascha W.
+ * last edit / by: 2020-01-24 / Sascha W.
+ */
+package de.hdm_stuttgart.mi.DungeonGame.Graphics;
+
+//Import statement
+import de.hdm_stuttgart.mi.DungeonGame.Graphics.Interfaces.IRenderable;
+
+/**
+ * Renderer responsible for generating the field graphics.
+ */
+public class FieldRenderer extends Renderer implements IRenderable {
+    
+    /**
+     * Method triggering the rendering process
+     */
+    @Override
+    public void render() {
+
+    }
+}
