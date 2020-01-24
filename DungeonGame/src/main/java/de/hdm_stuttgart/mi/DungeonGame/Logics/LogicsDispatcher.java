110c94807e479680d3758e41ce342e56cf655dc1
Sascha Wirtz <sw210@hdm-stuttgart.de>
2020-01-24 15:40:37 +0100

    created Logics-, and MainDispatecher class

---
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/LogicsDispatcher.java
@@ -0,0 +1,17 @@
+/*
+ * LogicsDispatcher.java
+ *
+ * Main logics class dispatching all logic processes
+ *
+ * author: Sascha W.
+ * last edit / by: 2020-01-24 / Sascha W.
+ */
+package de.hdm_stuttgart.mi.DungeonGame.Logics;
+
+//Import statements
+
+/**
+ * Main logics class dispatching all logic processes
+ */
+public class LogicsDispatcher {
+}
---
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/MainDispatcher.java
@@ -0,0 +1,17 @@
+/*
+ * MainDispatcher.java
+ *
+ * Main class dispatching all processes
+ *
+ * author: Sascha W.
+ * last edit / by: 2020-01-24 / Sascha W.
+ */
+package de.hdm_stuttgart.mi.DungeonGame;
+
+//Import statements
+
+/**
+ * Main class dispatching all processes
+ */
+public class MainDispatcher {
+}
