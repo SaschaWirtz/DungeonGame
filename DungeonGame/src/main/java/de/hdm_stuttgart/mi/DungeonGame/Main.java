aa4a57d4c34032636428bc3f982724524f90efee
Andreas Gantner <andreasgantner@yahoo.de>
2020-01-24 12:13:08 +0100

    Stubbing main entry point
    Stubbing the main entry point of the application.

--- DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Main.java
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Main.java
@@ -4,12 +4,15 @@
  * The main entry point for the game and related variables and methods.
  *
  * author: Moritz W.
- * last edit / by: 2019-12-09 / Andreas G.
+ * last edit / by: 2020-01-24 / Andreas G.
  */
 package de.hdm_stuttgart.mi.DungeonGame;
 
 //Import statements
 import de.hdm_stuttgart.mi.DungeonGame.Helper.Graphics.Console;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import java.io.Reader;
 
 /**
  * A small DungeonGame project. Class containing the main entry point for the game.
@@ -24,6 +27,22 @@ public class Main {
         //Ensuring that the screen is cleared. Please do not remove this line.
         Console.clear();
 
+        //The main key reader loop
+        try {
+            //Creating reader and terminal object and change terminal to raw mode
+            Terminal terminal = TerminalBuilder.terminal();
+            terminal.enterRawMode();
+            Reader reader = terminal.reader();
 
+            //The games main loop
+            while (true) {
+                //Read a new key code
+                int test = reader.read();
+
+                //ToDo: Handle the key code input
+            }
+        } catch (Exception e) {
+            //ToDo: Logic error handling
+        }
     }
 }
