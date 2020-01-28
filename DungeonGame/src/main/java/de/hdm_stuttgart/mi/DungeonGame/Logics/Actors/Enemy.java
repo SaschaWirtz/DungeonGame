57a28128dc2085d92f7930855f4ea5a9606918cd
dingi24 <moritz.wahlenmeier@yahoo.de>
2020-01-28 18:36:38 +0100

    Fix Enemy Movement

--- DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Actors/Enemy.java
+++ DungeonGame/src/main/java/de/hdm_stuttgart/mi/DungeonGame/Logics/Actors/Enemy.java
@@ -23,17 +23,6 @@ public class Enemy extends Actor {
     public void NextMove(){
         Coordinate playerPosition= Field.getPlayer().GetCoordinate();
         if(Field.getPlayer().GetLastMoveDirection()) {
-            if (coordinate.getxCoordinate() < playerPosition.getxCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate()+1,coordinate.getyCoordinate()).isWalkable()) {
-                coordinate.addxCoordinate(1);
-            } else if (coordinate.getxCoordinate() > playerPosition.getxCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate()-1,coordinate.getyCoordinate()).isWalkable()) {
-                coordinate.addxCoordinate(-1);
-            } else if (coordinate.getyCoordinate() < playerPosition.getyCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate(),coordinate.getyCoordinate()+1).isWalkable()) {
-                coordinate.addyCoordinate(1);
-            } else if (coordinate.getyCoordinate() > playerPosition.getyCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate(),coordinate.getyCoordinate()-1).isWalkable()) {
-                coordinate.addyCoordinate(-1);
-            }
-        }
-        else {
             if (coordinate.getyCoordinate() < playerPosition.getyCoordinate() && Field.getRoom().getFieldType(coordinate.getxCoordinate(), coordinate.getyCoordinate() + 1).isWalkable()) {
                 coordinate.addyCoordinate(1);
             } else if (coordinate.getyCoordinate() > playerPosition.getyCoordinate() && Field.getRoom().getFieldType(coordinate.getxCoordinate(), coordinate.getyCoordinate() - 1).isWalkable()) {
@@ -42,6 +31,17 @@ public class Enemy extends Actor {
                 coordinate.addxCoordinate(1);
             } else if (coordinate.getxCoordinate() > playerPosition.getxCoordinate() && Field.getRoom().getFieldType(coordinate.getxCoordinate() - 1, coordinate.getyCoordinate()).isWalkable()) {
                 coordinate.addxCoordinate(-1);
+            }
+        }
+        else {
+            if (coordinate.getxCoordinate() < playerPosition.getxCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate()+1,coordinate.getyCoordinate()).isWalkable()) {
+                coordinate.addxCoordinate(1);
+            } else if (coordinate.getxCoordinate() > playerPosition.getxCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate()-1,coordinate.getyCoordinate()).isWalkable()) {
+                coordinate.addxCoordinate(-1);
+            } else if (coordinate.getyCoordinate() < playerPosition.getyCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate(),coordinate.getyCoordinate()+1).isWalkable()) {
+                coordinate.addyCoordinate(1);
+            } else if (coordinate.getyCoordinate() > playerPosition.getyCoordinate()&&Field.getRoom().getFieldType(coordinate.getxCoordinate(),coordinate.getyCoordinate()-1).isWalkable()) {
+                coordinate.addyCoordinate(-1);
             }
         }
     }
