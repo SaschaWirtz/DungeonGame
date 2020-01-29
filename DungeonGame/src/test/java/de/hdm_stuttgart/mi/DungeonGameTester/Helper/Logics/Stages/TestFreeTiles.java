a3ef97849828235788bf4f73388dfece1bb05218
Sascha Wirtz <58302057+TrickyTreat@users.noreply.github.com>
2020-01-29 13:47:14 +0100

    Stage JUniTest update
    adding two tests to TestFreeTiles.java

--- DungeonGame/src/test/java/de/hdm_stuttgart/mi/DungeonGameTester/Helper/Logics/Stages/TestFreeTiles.java
+++ DungeonGame/src/test/java/de/hdm_stuttgart/mi/DungeonGameTester/Helper/Logics/Stages/TestFreeTiles.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
  */
 public class TestFreeTiles {
 
-    // A five by five field has 9 free tiles
+    // A five by five field has nine free tiles
     @Test
     public void nineFreeTilesInAFiveByFiveField() {
         FieldType[][] fieldType = new FieldType[5][5];
@@ -38,5 +38,39 @@ public class TestFreeTiles {
             }
         }
         Assert.assertTrue(floorCounter == 9);
+    }
+
+    // A five by ten field has twentyfour free tiles
+    @Test
+    public void twentyFourFreeTilesInAFiveByTenField() {
+        FieldType[][] fieldType = new FieldType[5][10];
+        new CreateWallAndFloor(fieldType);
+        ArrayList<Coordinate> freeTiles = FreeTiles.freeTiles(fieldType);
+        int floorCounter = 0;
+        for(int i = 0; i < fieldType.length; i++) {
+            for(int z = 0; z < fieldType[0].length; z++) {
+                if(fieldType[i][z] == FieldType.Floor) {
+                    floorCounter++;
+                }
+            }
+        }
+        Assert.assertTrue(floorCounter == 24);
+    }
+
+    // A twelve by seven field has fifty free tiles
+    @Test
+    public void fiftyFreeTilesInATwelveBySevenField() {
+        FieldType[][] fieldType = new FieldType[12][7];
+        new CreateWallAndFloor(fieldType);
+        ArrayList<Coordinate> freeTiles = FreeTiles.freeTiles(fieldType);
+        int floorCounter = 0;
+        for(int i = 0; i < fieldType.length; i++) {
+            for(int z = 0; z < fieldType[0].length; z++) {
+                if(fieldType[i][z] == FieldType.Floor) {
+                    floorCounter++;
+                }
+            }
+        }
+        Assert.assertTrue(floorCounter == 50);
     }
 }
